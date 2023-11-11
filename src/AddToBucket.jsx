8e7faa9efba8e644b30492f2b86b8e3b223f9bf1
Rafe Uddaraj <rafeuddaraj2@gmail.com>
2023-11-12 01:19:35 +0600

    Do not allow empty wish

--- src/AddToBucket.jsx
+++ src/AddToBucket.jsx
@@ -1,30 +1,35 @@
 import { createSignal } from "solid-js";
 
 export function AddToBucket(props) {
-  const [newItem, setNewItem] = createSignal('');
+    const [newItem, setNewItem] = createSignal("");
 
-  return (
-    <form>
-      <input
-        type="text"
-        placeholder="Make a wish"
-        value={newItem()}
-        onChange={(e) => {
-          setNewItem(e.target.value);
-        }}
-      />
-      <button
-        type="submit"
-        onClick={(e) => {
-          e.preventDefault();
-          props.setItems((items) => {
-            return [...items, { text: newItem(), complete: false }].reverse();
-          });
-          setNewItem('');
-        }}
-      >
-        Add
-      </button>
-    </form>
-  );
+    return (
+        <form>
+            <input
+                type="text"
+                placeholder="Make a wish"
+                value={newItem()}
+                onChange={(e) => {
+                    setNewItem(e.target.value);
+                }}
+            />
+            <button
+                type="submit"
+                onClick={(e) => {
+                    e.preventDefault();
+                    if (e.target.value) {
+                        props.setItems((items) => {
+                            return [
+                                ...items,
+                                { text: newItem(), complete: false },
+                            ].reverse();
+                        });
+                        setNewItem("");
+                    }
+                    alert(`You can't submit empty value`)
+                }}>
+                Add
+            </button>
+        </form>
+    );
 }
