12b2b1aacfa71efffac1dc4b177b8abd6201cd20
Mohim Khan <mohimkhan652@gmail.com>
2023-11-12 18:43:49 +0600

    Create Modal.jsx

---
+++ src/Modal.jsx
@@ -0,0 +1,37 @@
+export function Modal(props) {
+   return (
+      <div class="modal">
+         <div class="modal-dialog">
+            <div class="modal-content">
+               <div class="modal-header">
+                  <h5 class="modal-title">Modal title</h5>
+                  <button
+                     type="button"
+                     class="btn-close"
+                     onclick={()=>props.setIsOpen(false)}
+                  >X</button>
+               </div>
+               <div class="modal-body">
+                  <p>{props.text}</p>
+               </div>
+               <div class="modal-footer">
+                  <div class="btn-group">
+                      <button
+                         type="button"
+                         class="btn-confirm"
+                      onclick={()=> props.setConfirm(true)}>
+                         Yes
+                      </button>
+                      <button 
+                      type="button" 
+                      class="btn-reject"
+                      onclick={()=> props.setConfirm(false)}>
+                         No
+                      </button>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   );
+}
