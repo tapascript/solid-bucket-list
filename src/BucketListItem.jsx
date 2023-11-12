3aa4b65e3e8662ea9beaa0c0694f36bbf0d8d2e6
Towhidur-Rahman-Abid <abidrahman085@gmail.com>
2023-11-12 21:27:18 +0600

    (fect)add bucketlist edit functionality add bucketlist edit functionality and add necessary css
    issue #1

--- src/BucketListItem.jsx
+++ src/BucketListItem.jsx
@@ -1,4 +1,28 @@
+import { createSignal } from "solid-js";
+
 export function BucketListItem(props) {
+  const [isEditable, setIsEditable] = createSignal(false);
+  const [editableText, setEditableText] = createSignal("");
+
+  // Bucket text edit
+  const bucketTextEdit = () => {
+    if (editableText() === "") {
+      alert(`You can't submit empty value`);
+    } else {
+      props.setItems((items) => {
+        const newItems = items.map((item) =>
+          props.item === item ? { ...item, text: editableText() } : item
+        );
+        return newItems;
+      });
+    }
+  };
+
+  // Handle is editable
+  const handleIsEditable = () => {
+    setIsEditable(!isEditable());
+    setEditableText(props.item.text);
+  };
   return (
     <li
       class="list-item"
@@ -21,8 +45,34 @@ export function BucketListItem(props) {
             });
           }}
         />
-        {props.item.text}
+        {/* Item text */}
+        {isEditable() ? (
+          <input
+            type="text"
+            value={editableText()}
+            onChange={(e) => {
+              setEditableText(e.target.value);
+            }}
+          />
+        ) : (
+          props.item.text
+        )}
       </label>
+
+      {/* Buttons */}
+      {isEditable() ? (
+        <div>
+          <button onClick={bucketTextEdit}>Save</button>
+          <button onClick={handleIsEditable}>Cancel</button>
+        </div>
+      ) : (
+        <div>
+          {!props.item.complete && (
+            <button onClick={handleIsEditable}>Edit</button>
+          )}
+          <button>Delete</button>
+        </div>
+      )}
     </li>
   );
 }
