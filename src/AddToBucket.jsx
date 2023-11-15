ba93dd09f83fb7b67eee41e3cdac618738cfa90a
MD SAIFUL ISLAM <61042251+msisaif@users.noreply.github.com>
2023-11-15 20:18:21 +0600

    Install Tailwind and replace the CSS with tailwind #13 (#14)

--- src/AddToBucket.jsx
+++ src/AddToBucket.jsx
@@ -4,9 +4,10 @@ export function AddToBucket(props) {
   const [newItem, setNewItem] = createSignal('');
 
   return (
-    <form>
+    <form class="flex items-center gap-2">
       <input
         type="text"
+        class="w-60 border px-2 py-1.5 rounded-md text-xl"
         placeholder="Make a wish"
         value={newItem()}
         onChange={(e) => {
@@ -15,6 +16,7 @@ export function AddToBucket(props) {
       />
       <button
         type="submit"
+        class="px-3 py-1.5 text-xl rounded-md bg-blue-600 text-white"
         onClick={(e) => {
           e.preventDefault();
           props.setItems((items) => {
