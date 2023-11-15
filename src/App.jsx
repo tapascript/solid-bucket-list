e9777e98b7e20626e8966689612ebd6f86fb407e
Eth3R-One <refaythii@gmail.com>
2023-11-15 21:28:22 +0600

    Copied main 'package-lock.json' & npm install to resolve conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { createSignal } from 'solid-js';
-import { BucketListItem } from './BucketListItem';
-import { AddToBucket } from './AddToBucket';
+import { createSignal } from "solid-js";
+import { BucketListItem } from "./BucketListItem";
+import { AddToBucket } from "./AddToBucket";
 
-import {getWishes } from "./util/localStorageUtil"
-
-
+import { getWishes } from "./util/localStorageUtil";
 
 function App() {
   const [items, setItems] = createSignal(getWishes());
- 
+
   return (
     <div class="container">
       <h1>Solid Bucket List</h1>
@@ -18,7 +16,6 @@ function App() {
           {(item) => <BucketListItem item={item} setItems={setItems} />}
         </For>
       </ul>
-      
     </div>
   );
 }
