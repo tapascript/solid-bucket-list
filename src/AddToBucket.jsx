58f3f40d1f489995a53ecb10d9f0e61cd004ee60
Tanzil Islam <tanzil.ncpsc@gmail.com>
2023-11-18 01:16:51 +0600

    feat(#12): add date-time intergration for wish
    now each wishes will contain a deadline field which will show the user
    how much time left to complete the wish. also the creation time of wish
    is there to add more feature.
    
    Closes #12

--- src/AddToBucket.jsx
+++ src/AddToBucket.jsx
@@ -1,40 +1,76 @@
 import { createSignal } from "solid-js";
 
+import { isFutureDate } from "./util/dateTimeUtil";
 import { saveWish } from "./util/localStorageUtil";
 
 export function AddToBucket(props) {
-  const [newItem, setNewItem] = createSignal('');
+  const [newItem, setNewItem] = createSignal({
+    text: "",
+    deadline: null,
+  });
 
+  const handleWishDeadlineChange = (e) => {
+    const deadlineTime = e.target.value;
+
+    if (!isFutureDate(new Date(deadlineTime))) {
+      alert("You must pick a deadline time in the future.");
+      return;
+    }
+    setNewItem({ ...newItem(), deadline: deadlineTime });
+  };
+
+  const handleWishTextChange = (e) => {
+    setNewItem({ ...newItem(), text: e.target.value });
+  };
+
+  const isValidWish = (wish) => {
+    if (!wish.deadline) return false;
+    return true;
+  };
+  const handleWishCreate = (e) => {
+    e.preventDefault();
+
+    const newWish = newItem();
+
+    if (!isValidWish(newWish)) {
+      return alert("Please Enter valid wish deadline");
+    }
+
+    props.setItems((items) => {
+      const allWishes = [
+        {
+          id: crypto.randomUUID(),
+          text: newWish.text,
+          complete: false,
+          createdAt: new Date(),
+          deadline: newWish.deadline,
+        },
+        ...items,
+      ];
+      saveWish(allWishes);
+      return allWishes;
+    });
+    setNewItem("");
+  };
   return (
     <form class="flex items-center gap-2">
       <input
         type="text"
         class="w-60 border px-2 py-1.5 rounded-md text-xl"
         placeholder="Make a wish"
-        value={newItem()}
-        onChange={(e) => {
-          setNewItem(e.target.value);
-        }}
+        value={newItem().text}
+        onChange={handleWishTextChange}
+      />
+      <input
+        class="w-40 border px-2 py-1.5 rounded-md text-xl"
+        type="dateTime-local"
+        name="deadline"
+        onchange={handleWishDeadlineChange}
       />
       <button
         type="submit"
         class="px-3 py-1.5 text-xl rounded-md bg-blue-600 text-white"
-        onClick={(e) => {
-          e.preventDefault();
-          props.setItems((items) => {
-            const allWishes = [
-              {
-                id: crypto.randomUUID(),
-                text: newItem(),
-                complete: false,
-              },
-              ...items,
-            ];
-            saveWish(allWishes);
-            return allWishes;
-          });
-          setNewItem('');
-        }}
+        onClick={handleWishCreate}
       >
         Add
       </button>
