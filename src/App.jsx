74ced4b534530061ca04288d32a8dd2801c8c2ff
Minhajul Karim <project@zaagsys.com>
2023-12-10 12:04:12 +0600

    Add a component to clear done wishes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import { createSignal } from "solid-js";
 import { BucketListItem } from "./BucketListItem";
 import { AddToBucket } from "./AddToBucket";
+import { ClearDoneWishes } from "./ClearDoneWishes";
 
 import { getWishes } from "./util/localStorageUtil";
 
 function App() {
   const [items, setItems] = createSignal(getWishes());
 
-
   return (
     <div class="container flex flex-col justify-center items-center gap-4">
       <h1 class="text-4xl font-bold">Solid Bucket List</h1>
       <AddToBucket setItems={setItems} />
+      <ClearDoneWishes />
       <ul class="text-2xl">
         <For each={items()}>
           {(item) => <BucketListItem item={item} setItems={setItems} />}
---
+++ src/ClearDoneWishes.jsx
@@ -0,0 +1,7 @@
+export function ClearDoneWishes() {
+  return (
+    <button class="px-3 py-1.5 text-xl rounded-md bg-blue-600 text-white capitalize">
+      clear done wishes
+    </button>
+  );
+}
